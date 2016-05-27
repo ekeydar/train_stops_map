acbc1a1a250ca6c3e7f5dde8932301bd4b67c96d
Eran Keydar <ekeydar@gmail.com>
2016-05-27 15:54:46 +0300

    stops home

--- geo1/urls.py
+++ geo1/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('core:stops'), permanent=False)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^core/', include('core.urls', namespace='core')),
 ]
